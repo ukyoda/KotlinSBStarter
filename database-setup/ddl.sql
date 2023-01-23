d75e85383dce133870ea8645cd45f1b6dc3b1bb3
ukyoda <black12172001@hotmail.co.jp>
2023-01-23 16:56:09 +0900

    add start_docker.sh

---
+++ database-setup/ddl.sql
@@ -0,0 +1,7 @@
+CREATE TABLE user (
+    id int(10) NOT NULL,
+    name varchar(16) NOT NULL,
+    age int(10) NOT NULL,
+    profile varchar(64) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
